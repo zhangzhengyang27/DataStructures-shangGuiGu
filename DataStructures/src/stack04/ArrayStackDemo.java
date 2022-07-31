536530a063ee788dbf5dc18b1718789b8f279cc1
zhangzhengyang <1074385735@qq.com>
2022-07-31 18:59:29 +0800

    数组模拟栈

--- DataStructures/src/stack04/ArrayStackDemo.java
+++ DataStructures/src/stack04/ArrayStackDemo.java
@@ -1,16 +1,12 @@
 package stack04;
 
 public class ArrayStackDemo {
-}
-
-//定义一个 ArrayStack 表示栈
-class ArrayStack {
     private int maxSize; // 栈的大小
     private int[] stack; // 数组，数组模拟栈，数据就放在该数组
     private int top = -1;// top表示栈顶，初始化为-1
 
     //构造器
-    public ArrayStack(int maxSize) {
+    public ArrayStackDemo(int maxSize) {
         this.maxSize = maxSize;
         stack = new int[this.maxSize];
     }
