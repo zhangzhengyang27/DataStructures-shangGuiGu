22d6422d6713792bc30f9261491974dcf7e1caf7
zhangzhengyang <1074385735@qq.com>
2022-08-01 09:40:39 +0800

    排序算法

---
+++ DataStructures/src/sort06/sort.java
@@ -0,0 +1,334 @@
+package sort06;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class sort {
+
+    public static void main(String[] args) {
+
+        //创建要给80000个的随机的数组
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int) (Math.random() * 8000000); //生成一个[0, 8000000) 数
+        }
+
+        Date data1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(data1);
+        System.out.println("排序前的时间是=" + date1Str);
+
+        //测试冒泡排序
+        // bubbleSort(arr);
+        //测试选择排序
+        // selectSort(arr);
+
+        int[] arr1 = {101, 34, 119, 1, -1, 89};
+        // insertSort(arr1);
+        shellSort(arr1);
+        System.out.println(Arrays.toString(arr1));
+
+
+        Date data2 = new Date();
+        String date2Str = simpleDateFormat.format(data2);
+        System.out.println("排序后的时间是=" + date2Str);
+
+        // System.out.println("排序后");
+        // System.out.println(Arrays.toString(arr));
+
+    }
+
+
+    // 冒泡排序算法
+    public static void bubbleSort(int[] arr) {
+        int temp = 0; // 临时变量
+        boolean flag = false; // 标识变量，表示是否进行过交换
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    flag = true;
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+            if (!flag) { // 在一趟排序中，一次交换都没有发生过
+                break;
+            } else {
+                flag = false; // 重置flag!!!, 进行下次判断
+            }
+        }
+    }
+
+    //选择排序
+    public static void selectSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            int min = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                if (min > arr[j]) { // 说明假定的最小值，并不是最小
+                    min = arr[j]; // 重置min
+                    minIndex = j; // 重置minIndex
+                }
+            }
+
+            if (minIndex != i) {
+                arr[minIndex] = arr[i];
+                arr[i] = min;
+            }
+        }
+    }
+
+    // 插入排序
+    public static void insertSort(int[] arr) {
+        int insertVal = 0;   // 就是确定要插入的数据
+        int insertIndex = 0; // 就是确定要插入的下标
+        //使用for循环来把代码简化
+        for (int i = 1; i < arr.length; i++) {
+            insertVal = arr[i];
+            insertIndex = i - 1; // 往i下标的前面插入
+
+            // 给insertVal 找到插入的位置
+            // 说明
+            // 1. insertIndex >= 0 保证在给insertVal 找插入位置，不越界
+            // 2. insertVal < arr[insertIndex] 待插入的数，还没有找到插入位置
+            // 3. 就需要将 arr[insertIndex] 后移
+            // 如果要插入的数据比前面排序好的数据小，那么前面的数据要往后移
+            while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+                arr[insertIndex + 1] = arr[insertIndex]; // arr[insertIndex]
+                insertIndex--;
+            }
+            // 当退出while循环时，说明插入的位置找到, insertIndex + 1
+            // 举例：理解不了，我们一会 debug
+            //这里我们判断是否需要赋值
+            if (insertIndex + 1 != i) {
+                arr[insertIndex + 1] = insertVal;
+            }
+        }
+    }
+
+    // 使用逐步推导的方式来编写希尔排序
+    // 希尔排序时， 对有序序列在插入时采用交换法,
+    public static void shellSort(int[] arr) {
+
+        int temp = 0;
+        // 根据前面的逐步分析，使用循环处理
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                // 遍历各组中所有的元素(共gap组，每组有个元素), 步长gap
+                for (int j = i - gap; j >= 0; j -= gap) {
+                    // 如果当前元素大于加上步长后的那个元素，说明交换
+                    if (arr[j] > arr[j + gap]) {
+                        temp = arr[j];
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    //对交换式的希尔排序进行优化->移位法
+    public static void shellSort2(int[] arr) {
+
+        // 增量gap, 并逐步的缩小增量
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            // 从第gap个元素，逐个对其所在的组进行直接插入排序
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                if (arr[j] < arr[j - gap]) {
+                    while (j - gap >= 0 && temp < arr[j - gap]) {
+                        //移动
+                        arr[j] = arr[j - gap];
+                        j -= gap;
+                    }
+                    //当退出while后，就给temp找到插入的位置
+                    arr[j] = temp;
+                }
+
+            }
+        }
+    }
+
+    //  快速排序
+    public static void quickSort(int[] arr, int left, int right) {
+        int l = left; //左下标
+        int r = right; //右下标
+        //pivot 中轴值
+        int pivot = arr[(left + right) / 2];
+        int temp = 0; //临时变量，作为交换时使用
+        //while循环的目的是让比pivot 值小放到左边,比pivot 值大放到右边
+        while (l < r) {
+            //在pivot的左边一直找,找到大于等于pivot值,才退出
+            while (arr[l] < pivot) {
+                l += 1;
+            }
+            //在pivot的右边一直找,找到小于等于pivot值,才退出
+            while (arr[r] > pivot) {
+                r -= 1;
+            }
+            //如果l >= r说明pivot 的左右两的值，已经按照左边全部是
+            //小于等于pivot值，右边全部是大于等于pivot值
+            if (l >= r) {
+                break;
+            }
+
+            //交换
+            temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+
+            //如果交换完后，发现这个arr[l] == pivot值 相等 r--， 前移
+            if (arr[l] == pivot) {
+                r -= 1;
+            }
+            //如果交换完后，发现这个arr[r] == pivot值 相等 l++， 后移
+            if (arr[r] == pivot) {
+                l += 1;
+            }
+        }
+
+        // 如果 l == r, 必须l++, r--, 否则为出现栈溢出
+        if (l == r) {
+            l += 1;
+            r -= 1;
+        }
+        //向左递归
+        if (left < r) {
+            quickSort(arr, left, r);
+        }
+        //向右递归
+        if (right > l) {
+            quickSort(arr, l, right);
+        }
+    }
+
+    //合并的方法
+
+    /**
+     * @param arr   排序的原始数组
+     * @param left  左边有序序列的初始索引
+     * @param mid   中间索引
+     * @param right 右边索引
+     * @param temp  做中转的数组
+     */
+    public static void merge(int[] arr, int left, int mid, int right, int[] temp) {
+
+        int i = left; // 初始化i, 左边有序序列的初始索引
+        int j = mid + 1; //初始化j, 右边有序序列的初始索引
+        int t = 0; // 指向temp数组的当前索引
+
+        //(一)
+        //先把左右两边(有序)的数据按照规则填充到temp数组
+        //直到左右两边的有序序列，有一边处理完毕为止
+        while (i <= mid && j <= right) {//继续
+            //如果左边的有序序列的当前元素，小于等于右边有序序列的当前元素
+            //即将左边的当前元素，填充到 temp数组
+            //然后 t++, i++
+            if (arr[i] <= arr[j]) {
+                temp[t] = arr[i];
+                t += 1;
+                i += 1;
+            } else { //反之,将右边有序序列的当前元素，填充到temp数组
+                temp[t] = arr[j];
+                t += 1;
+                j += 1;
+            }
+        }
+
+        //(二)
+        //把有剩余数据的一边的数据依次全部填充到temp
+        while (i <= mid) { //左边的有序序列还有剩余的元素，就全部填充到temp
+            temp[t] = arr[i];
+            t += 1;
+            i += 1;
+        }
+
+        while (j <= right) { //右边的有序序列还有剩余的元素，就全部填充到temp
+            temp[t] = arr[j];
+            t += 1;
+            j += 1;
+        }
+
+
+        //(三)
+        //将temp数组的元素拷贝到arr
+        //注意，并不是每次都拷贝所有
+        t = 0;
+        int tempLeft = left; //
+        //第一次合并 tempLeft = 0 , right = 1 //  tempLeft = 2  right = 3 // tL=0 ri=3
+        //最后一次 tempLeft = 0  right = 7
+        while (tempLeft <= right) {
+            arr[tempLeft] = temp[t];
+            t += 1;
+            tempLeft += 1;
+        }
+    }
+
+    //基数排序方法
+    public static void radixSort(int[] arr) {
+
+        //根据前面的推导过程，我们可以得到最终的基数排序代码
+
+        //1. 得到数组中最大的数的位数
+        int max = arr[0]; //假设第一数就是最大数
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        //得到最大数是几位数
+        int maxLength = (max + "").length();
+
+
+        //定义一个二维数组，表示10个桶, 每个桶就是一个一维数组
+        //说明
+        //1. 二维数组包含10个一维数组
+        //2. 为了防止在放入数的时候，数据溢出，则每个一维数组(桶)，大小定为arr.length
+        //3. 名明确，基数排序是使用空间换时间的经典算法
+        int[][] bucket = new int[10][arr.length];
+
+        //为了记录每个桶中，实际存放了多少个数据,我们定义一个一维数组来记录各个桶的每次放入的数据个数
+        //可以这里理解
+        //比如：bucketElementCounts[0] , 记录的就是  bucket[0] 桶的放入数据个数
+        int[] bucketElementCounts = new int[10];
+
+
+        //这里我们使用循环将代码处理
+
+        for (int i = 0, n = 1; i < maxLength; i++, n *= 10) {
+            //(针对每个元素的对应位进行排序处理)， 第一次是个位，第二次是十位，第三次是百位..
+            for (int j = 0; j < arr.length; j++) {
+                //取出每个元素的对应位的值
+                int digitOfElement = arr[j] / n % 10;
+                //放入到对应的桶中
+                bucket[digitOfElement][bucketElementCounts[digitOfElement]] = arr[j];
+                bucketElementCounts[digitOfElement]++;
+            }
+            //按照这个桶的顺序(一维数组的下标依次取出数据，放入原来数组)
+            int index = 0;
+            //遍历每一桶，并将桶中是数据，放入到原数组
+            for (int k = 0; k < bucketElementCounts.length; k++) {
+                //如果桶中，有数据，我们才放入到原数组
+                if (bucketElementCounts[k] != 0) {
+                    //循环该桶即第k个桶(即第k个一维数组), 放入
+                    for (int l = 0; l < bucketElementCounts[k]; l++) {
+                        //取出元素放入到arr
+                        arr[index++] = bucket[k][l];
+                    }
+                }
+                //第i+1轮处理后，需要将每个 bucketElementCounts[k] = 0 ！！！！
+                bucketElementCounts[k] = 0;
+
+            }
+            //System.out.println("第"+(i+1)+"轮，对个位的排序处理 arr =" + Arrays.toString(arr));
+
+        }
+
+
+    }
+}
